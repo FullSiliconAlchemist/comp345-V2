b85854d1758815ab192c8f02f0bce6bd47a21f30
Rob <rdrury1513@gmail.com>
2019-11-04 16:00:13 -0500

    Added compute score method to player

--- PlayerDriver/PlayerDriver/Player.h
+++ PlayerDriver/PlayerDriver/Player.h
@@ -12,17 +12,20 @@ public:
 	bool payCoin(int amountToPay);
 	bool PlaceNewArmies(int numOfArmies, Country *countryToPlace); // Must accept a region on a map as a passed parameter to place armies (Map object)
 	int MoveArmies(int numOfMovements,int numToMove, Country *countryToTake, Country *countryToPlace); // Must accept a region on a map as a passed parameter to move armies
+	
 																									   // Charles: should also accept a map Object to verify that the movement is legal.
 	void MoveOverLand(int numToMove, Country *countryToTake, Country *countryToPlace); // Will probably be implemented within MoveArmies function
+
 	void BuildCity(Country *c);
 	bool DestroyArmy(int numOfArmies, Country *c);
 	void destroyArmymoveArmy(int numOfArmies, Country* c, int numOfMovements, int numToMove, Country* countryToTake, Country* countryToPlace);
 	void newArmymoveArmy(int numOfArmies, Country* countryToPlace, int numOfMovements, int numToMove, Country* countryToTake, Country* countryToMoveTo);
 	void citymoveArmy(Country* c, int numOfMovements, int numToMove, Country* countryToTake, Country* countryToPlace);
 	void ignore();
-
+	int computeScore(Map* m);
 	void bid();
 	void receiveGoldenCoins(int numOfCoins);
+	void showHand();
 
 	inline int GetArmyCubes() const { return *armyCubes; }	// Inline functions are defined in the header files
 	inline int GetCityDiscs() const { return *cityDiscs; }
