f7ef07557a4cb981c91b2fb526aaef2ea9b3fa2e
Rob <rdrury1513@gmail.com>
2019-11-18 17:00:22 -0500

    Merged gamestats/gameobserver into GameObserver and mapview/player view into GameView

--- PlayerDriver/PlayerDriver/GameObservers.cpp
+++ PlayerDriver/PlayerDriver/GameObservers.cpp
@@ -27,4 +27,29 @@ void SubjectPlayer::notify(Card c) {
 		(*i)->update(c);
 	}
 }
+GameStatisticsObserver::GameStatisticsObserver()
+{
+}
+
+GameStatisticsObserver::~GameStatisticsObserver()
+{
+}
+GameStatisticsSubject::GameStatisticsSubject() {
+	_Observers = new list<GameStatisticsObserver*>;
+}
+GameStatisticsSubject::~GameStatisticsSubject() {
+	delete _Observers;
+}
+void GameStatisticsSubject::Attach(GameStatisticsObserver* gso) {
+	_Observers->push_back(gso);
+};
+void GameStatisticsSubject::Detach(GameStatisticsObserver* gso) {
+	_Observers->remove(gso);
+};
+void GameStatisticsSubject::Notify() {
+	list<GameStatisticsObserver*>::iterator i = _Observers->begin(); // Starting index of iterator
+	for (; i != _Observers->end(); ++i)								 // No need for the first argument in for loop
+		(*i)->Update();
+};
+
 
--- PlayerDriver/PlayerDriver/GameView.cpp
+++ PlayerDriver/PlayerDriver/GameView.cpp
@@ -1,7 +1,27 @@
-#include "MapViewer.h"
-
+#include "GameView.h"
 #include <iostream>
+PlayerView::PlayerView()
+{
+}
+PlayerView::PlayerView(Player* p)
+{
+	_subject = p;
+	_subject->attach(this);
+}
 
+PlayerView::~PlayerView()
+{
+	_subject->detach(this);
+}
+void PlayerView::update(Card c) {
+	display(c);
+}
+void PlayerView::display(Card c) {
+	std::cout << "OBSERVER: Player " << _subject->GetId() << " Has " << _subject->GetGoldenCoins() << " coins and has picked up the card with the action " << c.getAction() << std::endl;
+	std::cout << "OBSERVER: That card costs " << c.getCost() << " which will leave Player " << _subject->GetId() << " with " 
+		<< _subject->GetGoldenCoins() - c.getCost() << " coins" << std::endl;
+	
+}
 using std::cout;
 using std::endl;
 
@@ -44,3 +64,4 @@ void MapViewer::display()
 		cout << " ]\n" << endl;
 	}
 }
+
